031fbb20e495401694d7bd9e9a5073b1b2178373
FF_Crazy <13453488571@163.com>
2025-07-23 19:17:26 +0800

    bulid: add project dependence

---
+++ deepcropmapping/__init__.py
@@ -0,0 +1,9 @@
+"""
+DeepCropMapping - Deep learning-based crop mapping using multi-temporal satellite imagery.
+
+This package provides tools and models for processing satellite imagery data
+and training deep learning models for crop type classification.
+"""
+
+__version__ = "0.1.0"
+__author__ = "DeepCropMapping Team"
